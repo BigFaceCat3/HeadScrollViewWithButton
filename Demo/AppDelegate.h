3e26e352700378fca54a5811ee254ed34abedf51
MacQiu <macqiu@macqiudemacbook-pro.local>
2015-12-29 10:16:54 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by MacQiu on 15/12/29.
+//  Copyright © 2015年 MacQiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by MacQiu on 15/12/29.
+//  Copyright © 2015年 MacQiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
